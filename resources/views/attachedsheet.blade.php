7ded498bd937ce0287439eee80213ba79b680cec
sleepingindomie <daffaprams09@gmail.com>
2024-08-01 13:05:34 +0700

    Pembenahan

--- resources/views/attachedsheet.blade.php
+++ resources/views/attachedsheet.blade.php
@@ -2,19 +2,18 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="">
-        <meta name="author" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
     
-        <title>Docsys - Attached Sheet</title>
+    <title>Docsys - Attached Sheet</title>
     
-        <link rel="icon" href="https://www.samudera.id/public_assets/img/logo-ico.jpg" type="image/x-icon">
-        <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link rel="icon" href="https://www.samudera.id/public_assets/img/logo-ico.jpg" type="image/x-icon">
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <style>
         body {
@@ -73,7 +72,7 @@
     </div>
     <div class="content">
         <h1>Attached Sheet</h1>
-        <p>{{ htmlspecialchars($data->attached_sheet_description) }}</p>
+        <p>{{ htmlspecialchars_decode($data->attached_sheet_description) }}</p>
     </div>
 </body>
 
