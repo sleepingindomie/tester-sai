848cbf4d6bc0e13c0e68bb886e01671cf21a79ae
sleepingindomie <82803431+sleepingindomie@users.noreply.github.com>
2025-06-12 17:55:33 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Https\Controllers;
+namespace App\Http\Controllers;
 
-use Illuminate\Https\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
